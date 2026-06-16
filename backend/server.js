769b527776ce9dc91af800154ca2707ef251ab4e
santhosh2852 <santhoshmetheofficial@gmail.com>
2026-06-16 15:03:31 +0530

    vercel fix vercel.json

--- backend/server.js
+++ backend/server.js
@@ -42,15 +42,15 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute); // Ensure postRoute is imported correctly
 app.use('/api/notifications', notificationRoute);
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, "dist")));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "dist")));
 
-//     app.use((req, res) => {
-//         res.sendFile(
-//             path.resolve(__dirname, "dist", "index.html")
-//         );
-//     });
-// }
+    app.use((req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "dist", "index.html")
+        );
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
